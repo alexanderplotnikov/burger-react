aa00af10c96051fb63146dcc429c230fdadb3cba
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-06-20 13:31:15 +0300

    burgerBuilder/BurgerIngridient/BurgerConstrols components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
-
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 const layout = (props) => (
-    <Aux>
+  <Aux>
     <div>Toolbar, sideDrawer, Backdrop</div>
-        <main>
-            {props.children}
-        </main>
-    </Aux>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
 );
 
 export default layout;
