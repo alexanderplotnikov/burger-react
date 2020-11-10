9b37ba5a8ed7e33b4b485c4830f23c6afcd2bd66
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-11-10 17:23:06 +0200

    add auth saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,11 +1,69 @@
-import { put } from 'redux-saga/effects';
-import * as actionTypes from '../actions/actionTypes';
+import { put, delay } from 'redux-saga/effects';
+import * as actions from '../actions/index';
+import axios from '../../axios-orders';
 
 export function* logoutSaga(action) {
   yield localStorage.removeItem('token');
   yield localStorage.removeItem('expirationDate');
   yield localStorage.removeItem('userId');
-  yield put({
-    type: actionTypes.AUTH_LOGOUT,
-  });
+  yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+  yield delay(action.expirationTime * 1000);
+  yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true,
+  };
+  let url =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAvQ2pELszvjCRNu63Z2q8bTESeLTcpYnc';
+
+  if (action.isSignup) {
+    url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAvQ2pELszvjCRNu63Z2q8bTESeLTcpYnc';
+  }
+
+  try {
+    const res = yield axios.post(url, authData);
+
+    const expirationDate = yield new Date(
+      new Date().getTime() + res.data.expiresIn * 1000
+    );
+    yield localStorage.setItem('token', res.data.idToken);
+    yield localStorage.setItem('expirationDate', expirationDate);
+    yield localStorage.setItem('userId', res.data.localId);
+    yield put(actions.authSuccess(res.data.idToken, res.data.localId));
+    yield put(actions.checkAuthTimeout(res.data.expiresIn));
+  } catch (err) {
+    yield put(actions.authFail(err.response.data.error));
+  }
+}
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem('token');
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date(
+      localStorage.getItem('expirationDate')
+    );
+    if (expirationDate > new Date()) {
+      const userId = yield localStorage.getItem('userId');
+      yield put(actions.authSuccess(token, userId));
+      yield put(
+        actions.checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
+    } else {
+      yield put(actions.logout());
+    }
+  }
 }
